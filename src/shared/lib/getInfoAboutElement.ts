ffac762b7c622a8cbd5a468748d68682821490f5
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-08-14 21:37:22 +0300

    Fix Regex in getInfoAboutElement function

--- src/shared/lib/getInfoAboutElement.ts
+++ src/shared/lib/getInfoAboutElement.ts
@@ -8,14 +8,15 @@ export enum TableCellColor {
   MILITARY_COLOR = "#1c523b",
 }
 
-const regex = /[А-К]-\d{3}/g;
+const auditoryRegex = /[А-К]-\d{3}/g;
+const LMSRegex = /LMS(|-\d+)/g;
 
 export const getInfoAboutElement = (element: string) => {
   if (element.length === 0) return TableCellColor.DEFAULT_COLOR;
   if (weekDays.includes(element.slice(0, 4))) return TableCellColor.DAY_COLOR;
-  if (element.includes("LMS")) return TableCellColor.LMS_COLOR;
+  if (element.match(LMSRegex)) return TableCellColor.LMS_COLOR;
   if (element.includes("Военная подготовка"))
     return TableCellColor.MILITARY_COLOR;
-  if (element.match(regex)) return TableCellColor.AUDIENCE_COLOR;
+  if (element.match(auditoryRegex)) return TableCellColor.AUDIENCE_COLOR;
   return TableCellColor.DEFAULT_COLOR;
 };
