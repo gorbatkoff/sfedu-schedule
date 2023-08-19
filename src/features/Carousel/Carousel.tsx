c25087c244e22a4b491177ef4fa1d14d76cfe477
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-08-19 17:38:57 +0300

    fix

--- src/features/Carousel/Carousel.tsx
+++ src/features/Carousel/Carousel.tsx
@@ -2,35 +2,32 @@ import { Button } from "@chakra-ui/react";
 import classNames from "classnames";
 import { useState, useEffect } from "react";
 
-import styles from './Carousel.module.scss'
+import styles from "./Carousel.module.scss";
 
 interface CarouselProps {
-    week: number;
-    carouselItems: number[],
-    fetchDataByWeek: (week:number)=>void
+  week: number;
+  carouselItems: number[];
+  fetchDataByWeek: (week: number) => void;
 }
 
-const Carousel = ({week,carouselItems,fetchDataByWeek}:CarouselProps) => {
-  
-    return (
-      <div className={classNames(styles.Carousel)}>
-        {carouselItems.map((item, index) => {;
-            return (
-                <Button className={styles.weekButton}
-                onClick={() => fetchDataByWeek(item)}
-                key={index}
-                isDisabled={week === index + 1}
-                colorScheme={
-                  week === index + 1 ? "green" : "twitter"
-                }
-                >
-                {item}
-              </Button>
-          )
-        })}
-      </div>
-    );
+const Carousel = ({ week, carouselItems, fetchDataByWeek }: CarouselProps) => {
+  return (
+    <div className={classNames(styles.Carousel)}>
+      {carouselItems.map((item, index) => {
+        return (
+          <Button
+            className={styles.weekButton}
+            onClick={() => fetchDataByWeek(item)}
+            key={index}
+            isDisabled={week === index + 1}
+            colorScheme={week === index + 1 ? "green" : "twitter"}
+          >
+            {item}
+          </Button>
+        );
+      })}
+    </div>
+  );
 };
-  
 
-export default Carousel
+export default Carousel;
