9eccc5b3f23d74156a0623f68b8c514c17484c96
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-08-23 21:03:46 +0300

    prettier fix

--- src/widgets/ScheduleCardsList/ui/ScheduleCardsList.tsx
+++ src/widgets/ScheduleCardsList/ui/ScheduleCardsList.tsx
@@ -20,14 +20,13 @@ interface TableProps {
 const ScheduleCardsList: FC<TableProps> = memo(
   ({ className, schedule, updateData }) => {
     const [day, setDay] = useState<number>(0);
-    
+
     useEffect(() => {
       const currentDay = new Date().getDay();
-      (0 < currentDay && currentDay < 7) ? setDay(currentDay - 1) : setDay(0)
-    },[])
+      0 < currentDay && currentDay < 7 ? setDay(currentDay - 1) : setDay(0);
+    }, []);
 
     const { colorMode } = useColorMode();
-
 
     const dayHandler = (index: number) => {
       setDay(index);
