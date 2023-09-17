2318528129db3c46bb4945536294f156d33a6065
gorbatkoff <gorbatkovartyom@gmail.com>
2023-09-17 16:13:40 +0300

    Update middleware

--- src/app/Providers/StoreProvider/config/store.ts
+++ src/app/Providers/StoreProvider/config/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
 import { userGroupReducer } from "/src/widgets/DrawerMenu/model/slice/userGroupSlice";
 import { tableReducer } from "/src/entities/ScheduleTable/model/slice/tableSlice";
 import { favoriteSearchReducer } from "/src/entities/ScheduleTable/model/slice/favoriteSearchSlice";
 import { selectVPKReducer } from "/src/features/SelectVPK/model/slice/selectVPKSlice";
-import { searchApi } from "/src/features/SearchSchedule/api";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { scheduleApi } from "/src/entities/ScheduleTable/api";
+import { searchApi } from "/src/features/SearchSchedule";
+import { scheduleApi } from "/src/entities/ScheduleTable";
+import { lessonsApi } from "/src/entities/UpcomingLessons";
 
 export const store = configureStore({
   reducer: {
@@ -15,11 +17,13 @@ export const store = configureStore({
     selectVPK: selectVPKReducer,
     [searchApi.reducerPath]: searchApi.reducer,
     [scheduleApi.reducerPath]: scheduleApi.reducer,
+    [lessonsApi.reducerPath]: lessonsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(searchApi.middleware)
-      .concat(scheduleApi.middleware),
+      .concat(scheduleApi.middleware)
+      .concat(lessonsApi.middleware),
 });
 
 setupListeners(store.dispatch);
