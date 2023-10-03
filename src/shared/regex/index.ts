8a57c5cefe90d52dfb64a182842369e6d6dc65ca
Artem Gorbatkov <97447842+gorbatkoff@users.noreply.github.com>
2023-10-03 22:38:07 +0300

    Add "ёЁ" to subgroup regex

--- src/shared/regex/index.ts
+++ src/shared/regex/index.ts
@@ -1,6 +1,6 @@
 export const auditoryLMSRegex = /([А-К]-\d{3})|(LMS(|-\d+))/g;
 export const subgroupRegex =
-  /([А-Яа-я]+ .\. .\.|\d+ п\/г * [А-Яа-я]+ .\. .\.)|[А-Яа-я0-9]+ ИКТИБ | Иностранный язык/g;
+  /([А-Яа-яёЁ]+ .\. .\.|\d+ п\/г * [А-Яа-яёЁ]+ .\. .\.)|[А-Яа-яёЁ0-9]+ ИКТИБ | Иностранный язык/g;
 export const groupRegex = /([А-Яа-я]{4}\d-\d+)/g;
 
 export const auditoryRegex = /[А-К]-\d{3}/g;
