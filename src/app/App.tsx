34262e4c84b2ac133a506ebb75f7845dded69316
gorbatkoff <gorbatkovartyom@gmail.com>
2023-12-13 23:01:15 +0300

    Remove about page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 
 import { Header } from "/src/widgets/Header";
 import { MainPage } from "/src/pages/MainPage";
-import { AboutPage } from "/src/pages/AboutPage";
 import { LeaveFeedbackPage } from "/src/pages/LeaveFeedback";
 import Loader from "/src/shared/ui/Loader/Loader";
 import { ThankYouPage } from "/src/pages/ThankYouPage";
@@ -15,7 +14,6 @@ const App = () => {
         <Header />
         <Routes>
           <Route element={<MainPage />} path="*" />
-          <Route element={<AboutPage />} path="/about" />
           <Route element={<ThankYouPage />} path="thanks-page" />
           <Route element={<LeaveFeedbackPage />} path="/leave-feedback" />
         </Routes>
