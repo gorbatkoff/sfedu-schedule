11209ec1d9e6cb306c77af073c311a8b17c94cd5
gorbatkoff <gorbatkovartyom@gmail.com>
2023-12-21 00:41:37 +0300

    Rollback changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { vitePwa } from "./src/shared/plugins/vitePwa";
+import { VitePWA } from "vite-plugin-pwa";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePwa()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#1e1e1e",
+        background_color: "#B100FF",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "Sfedu Schedule",
+        short_name: "Sfedu Schedule",
+        description:
+          "Sfedu Schedule это приложение помощник для просмотра расписания студентов ЮФУ",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
