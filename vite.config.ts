0e5016e6c5aae1e2d654761c763185161e495694
gorbatkoff <gorbatkovartyom@gmail.com>
2024-01-08 20:58:22 +0300

    Update README.md and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,50 +2,48 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const vitePwaConfig = {
+  workbox: {
+    globPatterns: ["**/*"],
+  },
+  // add this to cache all the
+  // static assets in the public folder
+  includeAssets: ["**/*"],
+  manifest: {
+    theme_color: "#1e1e1e",
+    background_color: "#B100FF",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    name: "Sfedu Schedule",
+    short_name: "Sfedu Schedule",
+    description:
+      "Sfedu Schedule это приложение помощник для просмотра расписания студентов ЮФУ",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+      },
+      {
+        src: "/icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
+
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      workbox: {
-        globPatterns: ["**/*"],
-      },
-      // add this to cache all the
-      // static assets in the public folder
-      includeAssets: ["**/*"],
-      manifest: {
-        theme_color: "#1e1e1e",
-        background_color: "#B100FF",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        name: "Sfedu Schedule",
-        short_name: "Sfedu Schedule",
-        description:
-          "Sfedu Schedule это приложение помощник для просмотра расписания студентов ЮФУ",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), VitePWA(vitePwaConfig)],
 });
