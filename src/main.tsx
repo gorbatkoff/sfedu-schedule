46c748c6238a7c51c69e436bee98aefdaae8f4aa
gorbatkoff <gorbatkovartyom@gmail.com>
2024-02-02 23:42:05 +0300

    Feat: remove react-snowfall library

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from "react";
 import { ChakraProvider, ThemeConfig, extendTheme } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Snowfall from "react-snowfall";
 import { registerSW } from "virtual:pwa-register";
 
 import App from "/src/app/App";
@@ -37,7 +36,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <BrowserRouter>
           <ChakraProvider theme={theme}>
             <App />
-            <Snowfall snowflakeCount={75} />
           </ChakraProvider>
         </BrowserRouter>
       </StoreProvider>
