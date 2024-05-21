640ffdb570426754a00b6409e5bbf2e15577207f
agorbatkov <agorbatkov@glowbyteconsulting.com>
2024-05-21 22:13:21 +0300

    fix: update required

--- src/main.tsx
+++ src/main.tsx
@@ -19,9 +19,7 @@ const theme: ThemeConfig = extendTheme({
 
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (confirm("Есть свежие обновления. Загрузить?")) {
-      updateSW(true);
-    }
+    updateSW(true);
   },
 });
 
