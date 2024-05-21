e162357a91375e1374dbe21e3c5d6377ec838f3a
agorbatkov <agorbatkov@glowbyteconsulting.com>
2024-05-21 21:53:10 +0300

    build: remove 144x144 icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { Display, VitePWA } from "vite-plugin-pwa";
 
 const vitePwaConfig = {
   workbox: {
@@ -12,7 +12,7 @@ const vitePwaConfig = {
   manifest: {
     theme_color: "#1e1e1e",
     background_color: "#1A202C",
-    display: "standalone",
+    display: "standalone" as Display,
     scope: "/",
     start_url: "/",
     name: "Sfedu Schedule",
@@ -33,11 +33,6 @@ const vitePwaConfig = {
       {
         src: "images/icons/icon-128x128.png",
         sizes: "128x128",
-        type: "image/png",
-      },
-      {
-        src: "images/icons/icon-144x144.png",
-        sizes: "144x144",
         type: "image/png",
       },
       {
