a819848040971bd8d2545b53cd9eae9940806e05
agorbatkov <agorbatkov@glowbyteconsulting.com>
2024-12-06 11:46:04 +0300

    Chiki-briki v damki

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { ChakraProvider, ThemeConfig, extendTheme } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Snowfall } from "react-snowfall";
 import { registerSW } from "virtual:pwa-register";
 
 import App from "/src/app/App";
@@ -30,6 +31,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <BrowserRouter>
           <ChakraProvider theme={theme}>
             <App />
+            <Snowfall
+              snowflakeCount={75}
+              style={{
+                position: "fixed",
+                width: "100vw",
+                height: "100vh",
+              }}
+            />
           </ChakraProvider>
         </BrowserRouter>
       </StoreProvider>
