4dd39f0f8b0e8f70e53580675fa40228ea59676d
agorbatkov <agorbatkov@glowbyteconsulting.com>
2025-04-08 00:24:51 +0300

    Change API url

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const __API_URL__ = "https://webictis.sfedu.ru/schedule-api";
+export const __API_URL__ = "https://shedule.rdcenter.ru/schedule-api";
 
 export const $api = axios.create({
   baseURL: __API_URL__,
